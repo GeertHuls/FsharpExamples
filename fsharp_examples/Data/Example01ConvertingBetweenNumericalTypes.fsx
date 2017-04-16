58c52de30f043012c0218c6f698dc1fc84c33ddf
Geert Huls <geert.huls82@gmail.com>
2017-04-16 22:01:07 +0200

    Numeric conversion examples

---
+++ fsharp_examples/Data/Example01ConvertingBetweenNumericalTypes.fsx
@@ -0,0 +1,18 @@
+
+// Does not compile
+// 1 + 3.14
+// Because 1 is an integer
+
+
+// Literal convertion:
+1. + 3.14
+
+// Or convert using funtcion:
+
+float 1
+
+// or convert string to float:
+float "3.14"
+
+//convert to decimal:
+decimal 3.14
