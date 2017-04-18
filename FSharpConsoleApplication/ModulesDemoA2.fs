954ac7f0afd1bef4c4630c0668c4f4f187554731
Geert Huls <geert.huls82@gmail.com>
2017-04-18 16:15:23 +0200

    Modules demo

---
+++ FSharpConsoleApplication/ModulesDemoA2.fs
@@ -0,0 +1,12 @@
+﻿// Modules are like namespace, except it contains value,
+// which holds functions.
+
+// A module at the top of the source file, applies to the
+// entire file.
+
+module A2
+
+type Person = {name:string}
+type Thing = {a:int}
+ 
+// --> B2
---
+++ FSharpConsoleApplication/ModulesDemoB2.fs
@@ -0,0 +1,9 @@
+﻿module ModulesDemoB2
+
+open A2
+
+type Person = {age:int; otherPerson: A2.Person}
+type ThingWrapper = {t:Thing}
+
+// This works because modules are allowed to contain values.
+let square x = x * x
