9773e29b142780d0296a02587fe477caa7bcd4ad
Geert Huls <geert.huls82@gmail.com>
2017-04-18 16:07:23 +0200

    Namespaces demo

---
+++ FSharpConsoleApplication/NamespaceDemo.fs
@@ -0,0 +1,35 @@
+﻿// Namespace examples, holding a set of types:
+namespace This.Is.MyNamespace
+type Class1() = 
+  member this.X = "F#"
+
+namespace This.Is.Another.MyNamespace
+type Class2() =
+  member this.Y = new This.Is.MyNamespace.Class1()
+
+
+// Or open namespace instead of fully qualified class name:
+
+namespace This.Is.Another.MyNamespace2
+
+open This.Is.MyNamespace
+
+type Class3() =
+  member this.Y = new Class1()
+
+
+namespace A
+
+type Person = {name:string}
+type Thing = {a:int}
+
+namespace B
+
+type Person = {age:int; otherPerson: A.Person}
+
+open A
+type ThingWrapper = {t:Thing}
+
+// This is a value, and won't compile in namespaces.
+// Use modules instead for this:
+//let square x = x * x // ERROR
--- FSharpConsoleApplication/Program.fs
+++ FSharpConsoleApplication/Program.fs
@@ -1,6 +1,21 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿
+// namespaces are the dotnet framework style solution for the
+// same as modules.
+
+// The difference is that namespaces cannot contain values,
+// only type declarations. This makes it more suitable
+// for OO code where namespaces will group a set of classes.
+
+// In a functional style, we would like to put functions in
+// a module and thus namespaces will not work.
+namespace FSharp.Examples
+
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+// modules are the ML style solution for grouping code and
+// managing name collisions.
+module MainFuncs = 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
