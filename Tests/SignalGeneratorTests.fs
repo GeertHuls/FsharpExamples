6d935025cef2610f7230a07889e613987afa04e0
Geert Huls <geert.huls82@gmail.com>
2017-04-19 21:56:01 +0200

    Added unit tests project

---
+++ Tests/SignalGeneratorTests.fs
@@ -0,0 +1,11 @@
+﻿module SignalGeneratorTests
+
+open NUnit.Framework
+open SignalGenerator
+
+[<TestFixture>]
+type ``When generating 2 seconds at 440Hz`` () =
+  [<Test>]
+  member this.``there should be 88200 samples`` () =
+    let samples = generateSamples 2000. 440.
+    Assert.AreEqual(8820, Seq.length samples)
