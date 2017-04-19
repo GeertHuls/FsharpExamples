2e397802fa2c6e055ae4766592d79431ed3f745b
Geert Huls <geert.huls82@gmail.com>
2017-04-19 21:03:20 +0200

    Access control demo

---
+++ FSharpConsoleApplication/AccessControlDemoA3.fs
@@ -0,0 +1,12 @@
+﻿// Modules are like namespace, except it contains value,
+// which holds functions.
+
+// A module at the top of the source file, applies to the
+// entire file.
+
+module A3
+
+type Person = {name:string}
+type private Thing = {a:int}
+ 
+// --> B2
---
+++ FSharpConsoleApplication/AccessControlDemoB3.fs
@@ -0,0 +1,9 @@
+﻿module ModulesDemoB3
+
+open A3
+
+type Person = {age:int; otherPerson: A3.Person}
+//type ThingWrapper = {t:Thing} // compile error, Thing is private
+
+// This works because modules are allowed to contain values.
+let square x = x * x
