8578325a00fa8271beb3d753fed37e39817b9dac
Geert Huls <geert.huls82@gmail.com>
2017-04-19 21:58:47 +0200

    Samples range test

--- Tests/SignalGeneratorTests.fs
+++ Tests/SignalGeneratorTests.fs
@@ -9,3 +9,9 @@ type ``When generating 2 seconds at 440Hz`` () =
   member this.``there should be 88200 samples`` () =
     let samples = generateSamples 2000. 440.
     Assert.AreEqual(8820, Seq.length samples)
+
+  [<Test>]
+  member this.``all samples should be in range`` ()=
+    let sixteenBitSampleLimit = 32767s
+    let samples = generateSamples 2000. 440.
+    samples |> Seq.iter (fun s -> Assert.IsTrue(s > (-1s * sixteenBitSampleLimit) && s < sixteenBitSampleLimit))
