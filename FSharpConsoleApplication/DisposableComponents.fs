ea69da8ad3ad987155aaf5787c3ad655e47e1b47
Geert Huls <geert.huls82@gmail.com>
2017-04-22 21:27:51 +0200

    Demo disposable components

---
+++ FSharpConsoleApplication/DisposableComponents.fs
@@ -0,0 +1,10 @@
+﻿module DisposableComponents
+
+open System.IO
+
+let readAFile () =
+  use reader = new StreamReader(
+    __SOURCE_DIRECTORY__ + "\\text.txt")
+  reader.ReadToEnd()
+
+readAFile()
