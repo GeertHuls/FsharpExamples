27ba4ae96d4342278a39a2ba913d29d101a349f9
Geert Huls <geert.huls82@gmail.com>
2017-04-22 22:07:55 +0200

    Property testing demo

---
+++ FSharpConsoleApplication/PropertyTesting.fs
@@ -0,0 +1,9 @@
+﻿module PropertyTesting
+
+//#r "packages/FsCheck.2.0.7/lib/net45/FsCheck.dll"
+//open FsCheck
+
+//let appendedListLength l1 l2 = 
+//    (l1 @ l2).Length = l1.Length + l2.Length
+    
+//Check.Quick appendedListLength 
