b8b26ded09bc9c7457810bdaf52cfd89f6db2773
Geert Huls <geert.huls82@gmail.com>
2017-12-17 22:11:33 +0100

    Find excel index by col number

--- fsharp_examples/Functions/Example13ParseExcelData.fsx
+++ fsharp_examples/Functions/Example13ParseExcelData.fsx
@@ -12,3 +12,75 @@ let someRes= '5' |> someLetterVal
 // Array.choose only selects the none - some values!
 // In the case below, the '9' char will be ommited!
 let chooseRes = [|'A'; '9'; 'B'|] |> Array.choose someLetterVal
+
+
+// Continue with the excel parsing exercise:
+let explode (s:string) =
+    [|for c in s -> c|]
+
+let letterValWithBug (c : char) =
+    match int c with
+    | v when v >= int 'A' && v <= int 'Z' -> Some (v - int 'A')
+    | v when v >= int 'a' && v <= int 'z' -> Some (v - int 'a')
+    | v when v >= int '0' && v <= int '9' -> Some (v - int '0')
+    | _ -> None
+
+// So this gets us a function which we can test out that should allow us to turn "A"
+// into the first column number, which when 0-based is 0:
+let colValue = 'A' |> letterValWithBug
+
+// Being an F# function that returns an Option, we can do something like the following:
+let colValues = "AB" |> explode |> Array.choose letterValWithBug
+
+// The problem with this is that we really need a single number, not two.
+// For AB we want to get 27, so we could build a function (colNum) that defines the following:
+let colNumBug =
+    explode
+    >> Array.choose letterValWithBug
+    >> Array.rev
+    >> Array.mapi (fun i x -> (float x) * System.Math.Pow(26., i |> float))
+    >> Array.sum
+    >> int
+
+colNumBug "BC" // Bug: returns 28!
+colNumBug "AB" // Bug: returns 1!
+
+// We have a huge bug with this though: for our value of "AB" it's going to return 1, not 27 as we would expect.
+// Our mistake is not so subtle: the (float x) * is the problem: when x is 0 (as it is for A), it will always return 0
+
+// We could fix this by doing float x + 1.
+let letterValThreePlacesFix (c : char) =
+    match int c with
+    | v when v >= int 'A' && v <= int 'Z' -> Some (1 + v - int 'A')
+    | v when v >= int 'a' && v <= int 'z' -> Some (1 + v - int 'a')
+    | v when v >= int '0' && v <= int '9' -> Some (1 + v - int '0')
+    | _ -> None
+
+
+// But now we think to ourselves, 'why am I doing the same step (1 + v - int ...) in three places?
+// Can I do that in one place?' We sure can:
+
+let letterVal (c : char) =
+    let f (c : char) v = 1 + v - int c
+    match int c with
+    | v when v >= int 'A' && v <= int 'Z' -> v |> f 'A' |> Some
+    | v when v >= int 'a' && v <= int 'z' -> v |> f 'a' |> Some
+    | v when v >= int '0' && v <= int '9' -> v |> f '0' |> Some
+    | _ -> None
+
+let sub x y = y - x
+let colNum =
+    explode
+    >> Array.choose letterVal
+    >> Array.rev
+    >> Array.mapi (fun i x -> (float x) * System.Math.Pow(26., i |> float))
+    >> Array.sum
+    >> int
+    >> sub 1
+
+
+colNum "BC" // Correct: 54
+colNum "AB" // Correct: 27
+
+// Applied to an entire array:
+["A"; "Z"; "AA"; "AB"; "ZZ"; "AAA"] |> List.map colNum //  [0; 25; 26; 27; 701; 702]
