45a5dacea8ca9db361634e27be118180c6b59bd3
Geert Huls <geert.huls82@gmail.com>
2018-01-11 21:27:20 +0100

    Moved working code from excel data parse fsi script into fsproject

---
+++ SpreadsheetReader/Excel.fs
@@ -0,0 +1,28 @@
+module Excel
+
+  let explode (s:string) =
+      [|for c in s -> c|]
+
+  let letterVal (c : char) =
+    let f (c : char) v = 1 + v - int c
+    match int c with
+    | v when v >= int 'A' && v <= int 'Z' -> v |> f 'A' |> Some
+    | v when v >= int 'a' && v <= int 'z' -> v |> f 'a' |> Some
+    | v when v >= int '0' && v <= int '9' -> v |> f '0' |> Some
+    | _ -> None
+
+  let sub x y = y - x
+
+  let baseValue b i =
+    System.Math.Pow(b, i)
+
+  let colNum =
+    let elementVal i x = i |> float |> baseValue 26. |> (*) x
+    explode
+    >> Array.choose letterVal
+    >> Array.rev
+    >> Array.map float
+    >> Array.mapi elementVal
+    >> Array.sum
+    >> int
+    >> sub 1
