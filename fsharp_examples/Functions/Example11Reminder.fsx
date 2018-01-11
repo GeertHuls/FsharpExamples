22f991d709b076ac7ffc25529da865b4919673b2
Geert Huls <geert.huls82@gmail.com>
2018-01-11 22:24:41 +0100

    Function composition tests

--- fsharp_examples/Functions/Example11Reminder.fsx
+++ fsharp_examples/Functions/Example11Reminder.fsx
@@ -19,8 +19,18 @@ let ebrown = {
 let createReminder thing user =
     sprintf "Hello %s, this is a reminder to do the thing (%s)." user.Name thing
 
+// given a function x : ('a -> 'b)
+// given a function y : ('b -> 'c)
+// the composition of x and y would be: ('a -> 'c)
 let remind =
     createReminder "Write a Blog Post" >> printfn "%s"
+
+
+let test2 =
+    createReminder "Write a Blog Post"
+
+let test =
+    printfn "%s"
 
 let reminder =
     ebrown |> remind
