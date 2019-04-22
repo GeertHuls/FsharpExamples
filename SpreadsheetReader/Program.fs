306b4157b03c343ed75a83e3975317ff0d9265e6
Geert Huls <geert.huls82@gmail.com>
2019-04-22 22:17:49 +0200

    Parsing console app parameters - add defaults when necessary

--- SpreadsheetReader/Program.fs
+++ SpreadsheetReader/Program.fs
@@ -1,10 +1,26 @@
 ﻿namespace SpreadsheetReader
 
+open System
 open System.IO;
 open NPOI.XSSF.UserModel;
 open NPOI.SS.UserModel;
 
+type Parameters = {
+    NewListFilename : string
+    OldListFilename : string
+    SourceSheetName : string
+}
+
 module Program =
+
+// http://usingprogramming.com/post/2017/09/01/getting-started-with-programming-and-getting-absolutely-nowhere-part-6
+// default args:
+  let fromDefault defaults args =
+    match args with
+    | [|newListFilename; oldListFilename; sourceSheetName|] -> Some { defaults with NewListFilename = newListFilename; OldListFilename = oldListFilename; SourceSheetName = sourceSheetName }
+    | [|newListFilename; oldListFilename|] -> Some { defaults with NewListFilename = newListFilename; OldListFilename = oldListFilename }
+    | [|newListFilename|] -> Some { defaults with NewListFilename = newListFilename }
+    | _ -> None
 
   let addSheet func (workbook : IWorkbook) name list =
     let header = { FirstName = "First Name"; MiddleName = Some "Middle Name"; LastName = "Last Name"; Email = "Email"; MagicId = "Magic ID" }
@@ -57,3 +73,29 @@ module Program =
     addSheet Merger.fillTemplateRow newBook templateSheetName newListUnique
 
     Merger.write newBook
+
+  [<EntryPoint>]
+  let main argv = 
+    let defaults = { NewListFilename = ""; OldListFilename = ""; SourceSheetName = "Process_Spec" }
+
+    let parameters =
+        match argv |> fromDefault defaults with
+        | Some p when p.NewListFilename <> "" && p.OldListFilename <> "" -> p
+        | Some p when p.NewListFilename <> "" ->
+            printf "Enter the old file name to load: "
+            { p with OldListFilename = Console.ReadLine() |> String.stripDQuotes }
+        | _ ->
+            printf "Enter the new file name to load: "
+            let temp = { defaults with NewListFilename = Console.ReadLine() |> String.stripDQuotes }
+            printf "Enter the old file name to load: "
+            { temp with OldListFilename = Console.ReadLine() |> String.stripDQuotes }
+
+    match argv |> fromDefault defaults with
+    | Some p when p.NewListFilename <> "" && p.OldListFilename <> "" ->
+        printfn "Done."
+    | _ ->
+        printfn "Done. Press enter to exit."
+        Console.ReadLine() |> ignore
+
+    printfn "Getting members not on new list."
+    0
